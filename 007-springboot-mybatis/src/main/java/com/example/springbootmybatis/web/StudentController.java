7db59f138e231909701a2287ead42c138e1d35db
mypower <mypower@163.com>
2022-06-07 19:58:45 +0800

    master commit

--- 007-springboot-mybatis/src/main/java/com/example/springbootmybatis/web/StudentController.java
+++ 007-springboot-mybatis/src/main/java/com/example/springbootmybatis/web/StudentController.java
@@ -21,4 +21,14 @@ public class StudentController {
 
         return student;
     }
+
+    @RequestMapping(value = "/springBoot/student2")
+    public @ResponseBody
+    Object student2() {
+
+        //岳岳
+        Student student = studentService.queryStudentById(1);
+
+        return student;
+    }
 }
